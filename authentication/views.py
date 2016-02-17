856f12ff17f370008341ffb80a67e3118008b38c
Tyler Crosse <tylerscottcrosse@gmail.com>
2016-02-17 12:28:32 -0500

    Added register feature

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import permissions, viewsets
 
-# Create your views here.
+from authentication.models import Account
+from authentication.permissions import IsAccountOwner
+from authentication.serializers import AccountSerializer
+
+class AccountViewSet(viewsets.ModelViewSet):
+    lookup_field = 'username'
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+
+    def get_permissions(self):
+        if self.request.method in permissions.SAFE_METHODS:
+            return (permissions.AllowAny(),)
+
+        if self.request.method == 'POST':
+            return (permissions.AllowAny(),)
+
+        return (permissions.IsAuthenticated(), IsAccountOwner(),)
---
+++ static/javascripts/thinkster.routes.js
@@ -0,0 +1,15 @@
+(function () {
+  angular
+    .module('thinkster.routes')
+    .config(config);
+
+  config.$inject = ['$routeProvider'];
+
+  function config($routeProvider) {
+    $routeProvider.when('/register', {
+      controller: 'RegisterController',
+      controllerAs: 'vm',
+      templateUrl: '/static/templates/authentication/register.html'
+    }).otherwise('/');
+  }
+})();
